c7f3dd857447015d7a60191252e9542f147309c9
MKJ <mkj.aldrin@icloud.com>
2023-09-27 22:31:37 +0200

    shadow parameters

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 class Base extends HTMLElement {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.attachShadow({ mode: "open" });
-    this.shadowRoot.innerHTML += `
+        this.attachShadow({ mode: "open" });
+        this.shadowRoot.innerHTML += `
     <style>
-        :host{
+
+        *{
+            box-sizing: border-box;
+        }
+
+        :host {
             display: flex;
             gap: 4px;
             flex-direction: column;
@@ -14,103 +19,251 @@ class Base extends HTMLElement {
 
             border: 1px currentColor solid;
         }
+
     </style>
     `;
 
-    this.addEventListener("com:bus", (e) => {
-      e.detail[this.constructor.name] = this;
-    });
-  }
+        this.addEventListener("com:bus", (e) => {
+            e.detail[this.constructor.name] = this;
+        });
 
-  connectedCallback() {
-    // this.emmitLifeCycle("connected");
-  }
+        this._init = false;
+    }
 
-  emmitLifeCycle(type) {
-    this.dispatchEvent(
-      new CustomEvent("com:bus", {
-        bubbles: true,
-        detail: {
-          emitter: this,
-        },
-      })
-    );
-  }
+    connectedCallback() {
+        this.emmitLifeCycle("connected");
+    }
+
+    emmitLifeCycle(type, emitter = this, composed = false) {
+        this.dispatchEvent(
+            new CustomEvent("com:bus", {
+                bubbles: true,
+                composed,
+                detail: {
+                    emitter,
+                },
+            })
+        );
+    }
+
+    get index() {
+        if (!this.parentElement) return -1;
+        const children = this.parentElement.children;
+
+        let i = 0;
+
+        for (const child of children) {
+            if (child == this) return i;
+            i++;
+        }
+
+        return -1;
+    }
+}
+
+export class COMNetwork extends Base {
+    constructor() {
+        super();
+
+        this.addEventListener("com:bus", (e) => {
+            if (e.detail.emitter instanceof COMParameter) {
+                const parameter = e.detail.emitter;
+                console.log(parameter);
+            }
+        });
+
+        this.shadowRoot.innerHTML += `
+        <x-flex row>
+            <slot></slot>
+        </x-flex>
+        `;
+    }
 }
 
 export class COMChain extends Base {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.shadowRoot.innerHTML += `
+        this.shadowRoot.innerHTML += `
     <x-flex>
         <slot></slot>
     </x-flex>
     `;
-  }
+    }
 }
+
+const MODULE_TYPES = {
+    PTH: [],
+    LFO: [
+        { name: "AMP", value: 0.5 },
+        { name: "FREQ", value: 0.125 },
+    ],
+    PROB: [{ name: "CHNS", value: 0.5 }],
+};
+
+/**@typedef {keyof typeof MODULE_TYPES} ModuleTypes */
 
 export class COMModule extends Base {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.shadowRoot.innerHTML += `
-    <span>PTH</span>
-    <div>
-        <input type="text" value="0.5" is="com-parameter" />
-    </div>
+        this.shadowRoot.innerHTML += `
+    <style>
+
+        input{
+            border: none;
+            max-width: 6ch;
+        }
+
+        #parameters {
+        }
+
+        #parameters:not(:has(*)){
+            display:none;
+        }
+
+        #outs {
+            padding: 4px;
+            background-color: red;
+        }
+
+
+    </style>
+
+    <span id="type">PTH</span>
+
+    <x-flex id="parameters"></x-flex>
+
+    <x-flex id="outs" row>
+        <slot></slot>
+    </x-flex>
     `;
-  }
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+
+        if (!this._init) {
+            /**@type {ModuleTypes} */
+            const type = this.getAttribute("type") ?? "PTH";
+            if (type == "PTH") return;
+            const parameters = MODULE_TYPES[type];
+
+            const ps = parameters.map((p, i) => {
+                /**@type {COMParameter} */
+                const pEl = document.createElement("com-parameter");
+                pEl.value = p.value;
+                pEl.name = p.name;
+
+                return pEl;
+            });
+
+            this.shadowRoot.getElementById("parameters").append(...ps);
+        }
+
+        this._init = true;
+    }
 }
 
-export class COMParameter extends HTMLInputElement {
-  constructor() {
-    super();
+export class COMParameter extends Base {
+    constructor() {
+        super();
 
-    this.addEventListener("change", (e) => {
-      this.dispatchEvent(
-        new CustomEvent("com:bus", {
-          bubbles: true,
-          composed: true,
-          detail: {
-            emitter: this,
-          },
-        })
-      );
-    });
-  }
+        this.shadowRoot.innerHTML += `
+        <style>
+            :host {
+                flex-direction: row;
+                align-items: center;
+            }
+
+            input {
+                font-size: inherit;
+                color: inherit;
+                font-family: inherit;
+                border: none;
+                width: 6ch;
+            }
+        </style>
+
+        <span id="name"></span>
+        <input type="test" value="0.5" />
+        `;
+
+        this.shadowRoot.addEventListener("change", (e) => {
+            this.emmitLifeCycle("change", this, true);
+        });
+    }
+
+    set value(v) {
+        this.shadowRoot.querySelector("input").value = v;
+    }
+
+    set name(n) {
+        this.shadowRoot.getElementById("name").textContent = n;
+    }
+}
+
+export class COMOut extends Base {
+    constructor() {
+        super();
+
+        this.shadowRoot.innerHTML += `
+        <style>
+            :host {
+                background-color: yellow;
+            }
+        </style>
+        <span>out</span>
+        `;
+    }
 }
 
 export class XFlex extends HTMLElement {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.attachShadow({ mode: "open" });
+        this.attachShadow({ mode: "open" });
 
-    this.shadowRoot.innerHTML += `
+        this.shadowRoot.innerHTML += `
     <style>
         :host{
             display: flex;
             flex-direction: column;
             gap: 4px;
+            flex-grow: 1;
+        }
+
+        :host([row]){
+            flex-direction:row;
         }
     </style>
     <slot></slot>
     `;
-  }
+    }
 }
 
 customElements.define("x-flex", XFlex);
 
+customElements.define("com-network", COMNetwork);
 customElements.define("com-chain", COMChain);
 customElements.define("com-module", COMModule);
-customElements.define("com-parameter", COMParameter, { extends: "input" });
+customElements.define("com-parameter", COMParameter);
+customElements.define("com-out", COMOut);
 
 document.body.innerHTML += `
-<com-chain>
-    <com-module></com-module>
-    <com-module></com-module>
-    <com-module></com-module>
-    <com-module></com-module>
-</com-chain>
+<com-network>
+    <com-chain>
+        <com-module type="LFO"></com-module>
+        <com-module type="PROB"></com-module>
+        <com-module>
+            <com-out></com-out>
+        </com-module>
+    </com-chain>
+    <com-chain>
+        <com-module></com-module>
+        <com-module>
+            <com-out></com-out>
+        </com-module>
+    </com-chain>
+</com-network>
 `;
